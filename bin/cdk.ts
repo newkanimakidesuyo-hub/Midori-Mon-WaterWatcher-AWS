77b2e0aa747cbe65b284aa0bae98068b8c4868e6
newkanimakidesuyo-hub <new.kanimakidesuyo@gmail.com>
2026-08-12 15:12:42 +0900

    update : スタック名の更新

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,9 +6,9 @@ import { GraphStack } from '../lib/graph/graph-stack';
 
 const app = new cdk.App();
 
-const foundation = new FoundationStack(app, 'FoundationStack');
-const device = new DeviceStack(app, 'DeviceStack');
-const graph = new GraphStack(app, 'GraphStack');
+const foundation = new FoundationStack(app, 'WaterWatcherFoundationStack');
+const device = new DeviceStack(app, 'WaterWatcherDeviceStack');
+const graph = new GraphStack(app, 'WaterWatcherGraphStack');
 
 device.addDependency(foundation);
 graph.addDependency(foundation);
