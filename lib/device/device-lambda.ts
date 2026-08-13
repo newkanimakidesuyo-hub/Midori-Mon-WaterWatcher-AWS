08c3df0efee785203c2d3190dbfd0b7246548895
newkanimakidesuyo-hub <new.kanimakidesuyo@gmail.com>
2026-08-13 09:59:46 +0900

    update : ログ出力ロジックの実装

--- lib/device/device-lambda.ts
+++ lib/device/device-lambda.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as logs from 'aws-cdk-lib/aws-logs';
 import * as path from 'path';
 
-export function createDeviceLambdas(stack: cdk.Stack) {
+export function createDeviceLambdas(stack: cdk.Stack, opts: { nodeLogGroup: logs.LogGroup; pyLogGroup: logs.LogGroup; nodeFunctionName: string; pyFunctionName: string; }) {
+  const { nodeFunctionName, pyFunctionName } = opts;
 
   new lambda.Function(stack, 'WaterWatcherNotificationCdkFunction', {
-    functionName: 'Midori-Mon-WaterWatcher-Notification-cdk',
+    functionName: nodeFunctionName,
     runtime: lambda.Runtime.NODEJS_18_X,
     handler: 'notification-handler.handler',
     code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda/device/notification')),
@@ -17,7 +19,7 @@ export function createDeviceLambdas(stack: cdk.Stack) {
   
   // Python version (imported from existing deployment) for testing
   new lambda.Function(stack, 'WaterWatcherNotificationCdkFunctionPy', {
-    functionName: 'Midori-Mon-WaterWatcher-Notification-cdk-py',
+    functionName: pyFunctionName,
     runtime: lambda.Runtime.PYTHON_3_9,
     handler: 'lambda_function.lambda_handler',
     code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda/device/notification_python')),
